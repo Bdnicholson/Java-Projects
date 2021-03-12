80eefc9fe8c7fa9e1820062a6e044fa84ba1e9e8
Bo Nicholson <41280077+Bdnicholson@users.noreply.github.com>
2021-03-12 15:48:10 -0600

    added via upload

---
+++ Proj1/Proj01.java
@@ -0,0 +1,25 @@
+/*
+Proj02 Copyright 2020 R.G.Baldwin
+ */
+
+
+/**
+ *
+ * @author pc
+ */
+//Class  Proj01 start here
+public class Proj01 {
+    //Main Function of the Program
+    public static void main(String[] args)
+    {
+        // \n is used for new line.
+       System.out.println(
+      "I certify that this program is my own work \n"+
+      "and is not the work of others. I agree not \n" +
+      "to share my solution with others.\n" +
+      "Bo Nicholson\n");
+      //Printing Hello World
+       System.out.println("Hello World");
+
+    }
+}//class Proj01 end here
