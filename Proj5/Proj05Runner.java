efc1893ed912aa767565565fb12f057d090792a6
Bo Nicholson <41280077+Bdnicholson@users.noreply.github.com>
2021-03-15 14:19:14 -0500

    Add files via upload

---
+++ Proj5/Proj05Runner.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author pc
+ */
+public class Proj05Runner extends Proj05{
+    public void run()
+    {
+        System.out.println("Bo Nicholoson");
+    }
+}
